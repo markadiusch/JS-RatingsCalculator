b37acf7281e3084c8c7869560658a77b69f6606d
markadiusch <hudzik.m@icloud.com>
2022-01-25 08:23:24 +0100

    Completed module

---
+++ js/ratings.js
@@ -0,0 +1,15 @@
+const elements = document.querySelectorAll('.rating');
+
+elements.forEach(element => {
+    //element.id.replace('star', );
+    console.log(parseInt(element.id));
+});
+
+function collect_ratings(){
+    let ratings = {
+        count: 0,
+        sum: 0,
+        average: 0
+    }
+    let rating = 0;
+};
