dd9920668b23b44b23ae5fe98a835f89b4eb52d6
markadiusch <hudzik.m@icloud.com>
2022-01-25 19:07:02 +0100

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -1,20 +1,24 @@
-const elements = document.querySelectorAll('.rating');
-
-elements.forEach(element => {
-    rating = parseInt(element.id.replace('star', ''));
-    ratings.count += parseInt(element.value);
-    ratings.sum += parseInt(element.value) * rating;
-});
-
-if (ratings.count !== 0) {
-    ratings.average = ratings.sum/ratings.count;
-};
-
-function collect_ratings(){
+const collect_ratings = () => {
     let ratings = {
         count: 0,
         sum: 0,
         average: 0
     }
     let rating = 0;
-};
+
+    elements.forEach(element => {
+        rating = parseInt(element.id.replace('star', ''));
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
+    });
+
+    if (ratings.count !== 0) {
+        ratings.average = ratings.sum/ratings.count;
+    };
+    
+}
+
+const elements = document.querySelectorAll('.rating');
+
+
+
